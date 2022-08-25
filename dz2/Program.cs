12bf186a6f175aaf5a743f9ef04ef9fdf8c12a44
ndzd <n_level@mail.ru>
2022-08-25 08:27:06 +0300

    dz2

---
+++ dz2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+System.Console.WriteLine("Введите три числа: ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int max1 = Math.Max(a, b);
+int max = Math.Max(max1, c);
+System.Console.WriteLine("Максимальное число = " + max);
